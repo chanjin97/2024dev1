0c29a3d7da02f63cae0b738bbabd6d938bbb92a0
chanjin97 <chanjindark@naver.com>
2024-07-11 20:10:05 +0900

    diarycontroller

--- startForest_Test_3/src/main/java/com/study/spring/controller/DiaryController.java
+++ startForest_Test_3/src/main/java/com/study/spring/controller/DiaryController.java
@@ -1,5 +1,14 @@
 package com.study.spring.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/diary")
+@RequiredArgsConstructor
 public class DiaryController {
 
+	
 }
